62fc3174bf57cf77de3ac6a96ed43f781b5f4a7a
Admin <Admin@DESKTOP-O2C96HO>
2023-03-24 19:00:07 +0530

    Core Java

---
+++ Learning/src/Fundamentals/Hello_world.java
@@ -0,0 +1,14 @@
+package Fundamentals;
+
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		
+		System.out.println("Hello world");
+		//sysout space ctrl
+		System.out.println("Hello world");
+	}
+
+}
